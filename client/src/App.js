693fd199b19b9340a7a81a639be4cb669302b04a
thisisbrix901 <bmadeira7@gmail.com>
2018-11-14 17:30:05 -0500

    components building

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 import SideBar from "./components/SideBar"
 import NavBar from "./components/NavBar"
 import MessageInput from "./components/MessageInput"
+import ChatLog from "./components/ChatLog"
+import Wrapper from "./components/Wrapper"
+
 
 class App extends Component {
   render() {
     return (
       <div>
+
+        <Wrapper>
         <SideBar />
         <NavBar />
         <MessageInput />
-        
+        <ChatLog />
+        </Wrapper>
         </div>
         );
   }
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -5,39 +5,13 @@ const NavBar = () => (
   
       <nav className="nav-extended teal header">
         <div className="nav-wrapper">
-          <a href="#" className="brand-logo">
-            App Name
-          </a>
-          {/* <!-- <a href="something.html" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a> --> */}
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <a href="sass.html">Sass</a>
-            </li>
-            <li>
-              <a href="badges.html">Components</a>
-            </li>
-            <li>
-              <a href="collapsible.html">JavaScript</a>
-            </li>
-          </ul>
+        <span className="settings">⚙️</span>
+            <p className="comet">Comet</p> 
+          <p className="shortMsg">A corporate messaging app</p>
+          
         </div>
         <div className="nav-content">
-          <ul className="tabs tabs-transparent">
-            <li className="tab">
-              <a href="#test1">Tab 1</a>
-            </li>
-            <li className="tab">
-              <a href="#test2">
-                Tab 2
-              </a>
-            </li>
-            <li className="tab">
-              <a href="#test3">Tab 3</a>
-            </li>
-            <li className="tab">
-              <a href="#test4">Tab 4</a>
-            </li>
-          </ul>
+          
         </div>
       </nav>
    
