77ced9c3a6ccb39cd8a125648db8b3b473962a3f
thisisbrix901 <bmadeira7@gmail.com>
2018-11-14 21:35:58 -0500

    modal, react routes for home page, signup and login

--- client/src/components/SideBar/SideBar.js
+++ client/src/components/SideBar/SideBar.js
@@ -1,28 +1,48 @@
 import React from "react";
 import "./SideBar.css";
+import { Modal, Button } from "react-materialize";
 
-const SideBar = () => (
-  <div>
-    <div id="mainRow" className="card col s4 m2 l2">
-      <h5 className="groupMessages">Group Messages</h5>
-      <div className="groupMessageDiv">Channel 1</div>
-      <div className="groupMessageDiv">Channel 2</div>
-      <div className="groupMessageDiv">Channel 3</div>
-      
 
-      <h5 className="directMessages">Direct Messages</h5>
+class SideBar extends React.Component  {
+createChat = () =>{
 
-      <div className="directMessageDiv">Jeremy Shults</div>
-      <div className="directMessageDiv">Jacob Broughton </div>
-      <div className="directMessageDiv">Oprah Winfrey</div>
-      <div className="directMessageDiv">Dwayne Johnson</div>
-      <div className="directMessageDiv">Jake Gyllenhall</div>
-      
-  <button className="btn waves-effect waves-light newConvoBtn" type="submit" name="action">Start A New Conversation
+}
+
+render() {
+  return (
+    <div>
+
+      <div id="mainRow" className="card col s4 m2 l2">
+        <h5 className="groupMessages">Group Messages</h5>
+        <div className="groupMessageDiv">Channel 1</div>
+        <div className="groupMessageDiv">Channel 2</div>
+        <div className="groupMessageDiv">Channel 3</div>
+
+        <h5 className="directMessages">Direct Messages</h5>
+
+        <div className="directMessageDiv">Jeremy Shults</div>
+        <div className="directMessageDiv">Jacob Broughton </div>
+        <div className="directMessageDiv">Oprah Winfrey</div>
+        <div className="directMessageDiv">Dwayne Johnson</div>
+        <div className="directMessageDiv">Jake Gyllenhall</div>
+
+      <Modal
+        header="Who do you want to message ?"
+        trigger={<Button className="newConvoBtn btn waves-effect waves-light">Start a Conversation</Button>}
+      >
+        <label> Create A New Message
+        <input placeholder="Username" id="usernameInput" type="text" name="userName"/>
+        </label>
+        <button className="btn waves-effect waves-light" type="submit" >Start Message</button>
+        {/* onClick={this.} */}
+      </Modal>
+        {/* <button className="btn waves-effect waves-light newConvoBtn" type="submit" name="action" onClick={this.#}>Start A New Conversation
   
-  </button>
+  </button> */}
+      </div>
     </div>
-  </div>
-);
+  );
+};
+}
 
 export default SideBar;
