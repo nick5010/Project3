8d115c5e2041228323c6b357dc49559a3235f5d6
thisisbrix901 <bmadeira7@gmail.com>
2018-11-19 14:45:15 -0500

    working on UI

--- client/src/components/DirectMessage/DirectMessage.js
+++ client/src/components/DirectMessage/DirectMessage.js
@@ -1,14 +1,16 @@
 import React from "react";
+import "./DirectMessage.css"
 class DirectMessage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       directMessageLoaded: true
+
     };
   }
 
   render() {
-    return <div>{this.state.directMessageLoaded}</div>;
+    return <div className="chatUser">{this.props.user}</div>;
   }
 }
 
