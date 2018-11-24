9d1c7bada036b0b4cdc208bc682b053cf3aad629
jlb1999 <JacobBroughton@JacobBroughtonsMacbook.local>
2018-11-24 13:33:15 -0500

    notDoneStyle push

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,20 +1,27 @@
 import React from "react";
 import "./NavBar.css";
 
-const NavBar = () => (
-  
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoURL: "../public/images/Background_08.mov"
+    }
+  }
+
+  render() {
+    return (
       <nav className="nav-extended header">
         <div className="nav-wrapper">
-        <i className="settings fa fa-cog fa-spin"></i>
-            <span className="comet">Comet</span> 
+          <i className="settings fa fa-cog fa-spin"></i>
+          <span className="comet">Comet</span>
           <p className="shortMsg">A corporate messaging app</p>
-          
         </div>
         <div className="nav-content">
-          
         </div>
       </nav>
-   
-);
+    )
+  }
+};
 
 export default NavBar;
